c6c817eb88f5ddf4835c1ea7a820ee57ac4b6119
maxro <maxro@LAPTOP-0U1677N5.mshome.net>
2022-10-07 09:46:55 +0200

    "Suite TP2"

--- src/personnage/Village.java
+++ src/personnage/Village.java
@@ -2,13 +2,24 @@ package personnage;
 
 public class Village {
 	private String nom;
-
-	public Village(String nom) {
-		super();
+	private Chef chef;
+	private int nbVillageois;
+	private Gaulois[] villageois;
+	
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+	}
+	
+	public void setchef(Chef chef) {
+		this.chef = chef;
 	}
 	
 	public String getNom() {
 		return nom;
+	}
+	
+	public AjouterHabitant(Gaulois gaulois) {
+		this.Gaulois[nbVillageoisMaximum] = gaulois;
+		
 	}
 }
